5c1ce4622303febf9b10b4adcc16fc7fba503c57
Jason Livesay <runvnc@gmail.com>
2024-05-05 15:00:09 +0000

    update

--- face_swap.py
+++ face_swap.py
@@ -1,7 +1,7 @@
 from facefusionlib import swapper
 from facefusionlib.swapper import DeviceProvider
 
-def face_swap(input_image_path, target_image_path, skip_nsfw=False)
+def face_swap(input_image_path, target_image_path, skip_nsfw=False): 
     result = swapper.swap_face(
             source_paths=[input_image_path],
             target_path=target_image_path,
@@ -12,4 +12,8 @@ def face_swap(input_image_path, target_image_path, skip_nsfw=False)
             landmarker_score=0.5
         )
 
-print(result)
+    print(result)
+
+if __name__ == "__main__":
+    face_swap("imgs/input.png", "imgs/target.png", skip_nsfw=True)
+
