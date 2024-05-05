03bc36d1e0a1e074aeec661c2e646818c3981d6e
Jason Livesay <runvnc@gmail.com>
2024-05-05 16:22:53 +0000

    update

--- sdxl.py
+++ sdxl.py
@@ -6,16 +6,17 @@ from nanoid import generate
 def random_img_fname():
     return generate() + ".png"
 
-def sdxl_text_to_image(prompt, model_id, count=1, save_to="imgs/" + random_img_fname(), w=1024, h=1024, steps=20, cfg=8):
+def sdxl_text_to_image(model_id, prompt, negative_prompt, count=1, save_to="imgs/" + random_img_fname(), w=1024, h=1024, steps=20, cfg=8):
     pipeline = StableDiffusionXLPipeline.from_single_file(model_id, torch_dtype=torch.float16)
     pipeline = pipeline.to("cuda")
 
     for n in range(1, count+1):
-        image = pipeline(prompt).images[0]
+        image = pipeline(prompt=prompt, negative_prompt=negative_prompt, guidance_scale=cfg).images[0]
         image.save("imgs/"+random_img_fname())
 
 if __name__ == "__main__":
     prompt = sys.argv[1]
-    sdxl_text_to_image(prompt,"models/model.safetensors", count=2)
+    negative_prompt = sys.argv[2]
+    sdxl_text_to_image("models/model.safetensors", prompt, negative_prompt, count=2, cfg=30)
 
 
