8846aec1517061b435d6ad15b5deffd94a50cf49
Jason Livesay <runvnc@gmail.com>
2024-05-05 14:56:07 +0000

    update

--- sdxl.py
+++ sdxl.py
@@ -5,16 +5,16 @@ from nanoid import generate
 def random_img_fname():
     return generate() + ".png"
 
-def sdxl_text_to_image(prompt, model_id, save_to=random_img_fname(), w=1024, h=1024, steps=20, cfg=8):
+def sdxl_text_to_image(prompt, model_id, count=1, save_to="imgs/" + random_img_fname(), w=1024, h=1024, steps=20, cfg=8):
     pipeline = StableDiffusionXLPipeline.from_single_file(model_id, torch_dtype=torch.float16)
     pipeline = pipeline.to("cuda")
 
-    image = pipeline(prompt).images[0]
-
-    image.save(save_to)
+    for n in range(1, count+1):
+        image = pipeline(prompt).images[0]
+        image.save("imgs/"+random_img_fname())
 
 if __name__ == "__main__":
-    prompt = "a beautiful sunset over a tranquil beach, vibrant colors, detailed, artstation, 4k"
-    sdxl_text_to_image(prompt,"stabilityai/stable-diffusion-xl-base-1.0")
+    prompt = "a beach at sunset"
+    sdxl_text_to_image(prompt,"models/model.safetensors", count=2)
 
 
