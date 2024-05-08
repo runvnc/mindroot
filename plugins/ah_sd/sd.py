db1a0d6e43e19640824db61d6c69b4b1083a0223
Jason Livesay <runvnc@gmail.com>
2024-05-08 11:51:42 -0500

    update

--- plugins/ah_sd/sd.py
+++ plugins/ah_sd/sd.py
@@ -3,22 +3,32 @@ import sys
 import asyncio
 from diffusers import StableDiffusionXLPipeline, StableDiffusionPipeline
 from nanoid import generate
+import os
 
-current_model = 'manpreets7/dreamshaper-3.2'
+if os.environ.get('AH_DEFAULT_SD_MODEL'):
+    current_model = os.environ.get('AH_DEFAULT_SD_MODEL')
+    local_model = True
+else:
+    current_model = 'manpreets7/dreamshaper-3.2'
+    local_model = False
 
 def random_img_fname():
     return generate() + ".png"
 
-def use_model(model_id):
+def use_model(model_id, local=True):
     global current_model
+    global local_model
     current_model = model_id
+    local_model = local
 
-
-async def sdxl_text_to_image(prompt, negative_prompt='', model_id=None, from_huggingface=True,
+async def sdxl_text_to_image(prompt, negative_prompt='', model_id=None, from_huggingface=None,
                              count=1, save_to="imgs/" + random_img_fname(), w=1024, h=1024, steps=20, cfg=8):
     if model_id is None:
         model_id = current_model
  
+    if from_huggingface is None:
+        from_huggingface = local_model
+
     if not from_huggingface:
         pipeline = StableDiffusionXLPipeline.from_single_file(model_id, torch_dtype=torch.float16)
     else:
@@ -34,10 +44,15 @@ async def sdxl_text_to_image(prompt, negative_prompt='', model_id=None, from_hug
         image.save(fname)
         return fname
 
-async def sd_text_to_image(prompt, negative_prompt='', model_id=None, from_huggingface=True, count=1,
+async def sd_text_to_image(prompt, negative_prompt='', model_id=None, from_huggingface=None, count=1,
                            save_to="imgs/" + random_img_fname(), w=512, h=512, steps=20, cfg=8):
     if model_id is None:
         model_id = current_model
+
+    if from_huggingface is None:
+        from_huggingface = local_model
+
+
     if not from_huggingface:
         pipeline = StableDiffusionPipeline.from_single_file(model_id, torch_dtype=torch.float16)
     else:
