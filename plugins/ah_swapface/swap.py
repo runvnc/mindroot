a41785128d013d31d70a91a890ce4677e20bca7b
Jason Livesay <runvnc@gmail.com>
2024-05-10 08:06:18 -0500

    update

--- plugins/ah_swapface/swap.py
+++ plugins/ah_swapface/swap.py
@@ -24,7 +24,7 @@ class FaceSwap:
             root="models/face"
         )
         self.swap_model = INSwapper(
-            "models/face",
+            "models/face/inswapper_128.onnx",
             onnxruntime.InferenceSession(
                 path_or_bytes='models/face/inswapper_128.onnx',
                 providers=onnxruntime.get_available_providers(),
@@ -34,10 +34,10 @@ class FaceSwap:
     def check_for_models(self):
         model_path = Path("models/face/inswapper_128.onnx")
         if not model_path.exists():
-            log.info("Models not found, downloading them")
+            print("Models not found, downloading them")
             self.download_models()
         else:
-            log.info("Models found, skipping download")
+            print("Models found, skipping download")
 
     def download_models(self):
         download_and_extract("https://github.com/deepinsight/insightface/releases/download/v0.7/buffalo_l.zip", "models/face")
@@ -51,7 +51,7 @@ class FaceSwap:
         face = self.face_analyser.get(img_data)
 
         if len(face) == 0 and det_size[0] > 320 and det_size[1] > 320:
-            log.debug("No face detected, trying again with smaller image")
+            print.debug("No face detected, trying again with smaller image")
             det_size_half = (det_size[0] // 2, det_size[1] // 2)
             return self.get_face_single(
                 img_data,
@@ -73,38 +73,38 @@ class FaceSwap:
     ) -> Image.Image:
         if faces_index is None:
             faces_index = {0}
-        log.debug(f"Swapping faces: {faces_index}")
+        print(f"Swapping faces: {faces_index}")
         result_image = target_img
 
-        if face_swapper_model is not None:
+        if self.swap_model is not None:
             cv_source_img = cv2.cvtColor(np.array(source_img), cv2.COLOR_RGB2BGR)
             cv_target_img = cv2.cvtColor(np.array(target_img), cv2.COLOR_RGB2BGR)
-            source_face = get_face_single(
-                face_analyser, cv_source_img, face_index=0
+            source_face = self.get_face_single(
+                self.face_analyser, cv_source_img, face_index=0
             )
             if source_face is not None:
                 result = cv_target_img
 
                 for face_num in faces_index:
-                    target_face = get_face_single(
-                        face_analyser, cv_target_img, face_index=face_num
+                    target_face = self.get_face_single(
+                        self.face_analyser, cv_target_img, face_index=face_num
                     )
                     if target_face is not None:
                         sys.stdout = NullWriter()
-                        result = face_swapper_model.get(
+                        result = self.swap_model.get(
                             result, target_face, source_face
                         )
                         sys.stdout = sys.__stdout__
                     else:
-                        log.warning(f"No target face found for {face_num}")
+                        print(f"No target face found for {face_num}")
 
                 result_image = Image.fromarray(
                     cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
                 )
             else:
-                log.warning("No source face found")
+                print("No source face found")
         else:
-            log.error("No face swap model provided")
+            print("No face swap model provided")
         return result_image
 
 def __init__():
@@ -122,11 +122,15 @@ def swap_face(src_dir, target_img_filepath):
             src_images.append(src_img)
     
     if len(src_images) > 0:
-        swapped_img = swapper.swap_face(src_images, target_img)
+        swapped_img = swapper.swap_face(src_images[0], target_img)
         
         output_filename = "swapped_result.jpg"
         output_path = os.path.join(src_dir, output_filename)
         swapped_img.save(output_path)
     else:
         print("No source images found in the directory.")
+
+if __name__ == "__main__":
+    __init__()
+    swap_face("imgs/faceref/g", "imgs/target.png")
 
