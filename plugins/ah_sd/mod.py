46ecb0fc453fa8c77225b20ee0225afc76fe5542
Jason Livesay <runvnc@gmail.com>
2024-05-26 16:48:45 -0500

    update

--- plugins/ah_sd/mod.py
+++ plugins/ah_sd/mod.py
@@ -36,6 +36,43 @@ def use_model(model_id, local=True):
     current_model = model_id
     local_model = local
 
+
+
+def get_pipeline_embeds(pipeline, prompt, negative_prompt, device):
+    """ Get pipeline embeds for prompts bigger than the maxlength of the pipe
+    :param pipeline:
+    :param prompt:
+    :param negative_prompt:
+    :param device:
+    :return:
+    """
+    max_length = pipeline.tokenizer.model_max_length
+
+    # simple way to determine length of tokens
+    count_prompt = len(prompt.split(" "))
+    count_negative_prompt = len(negative_prompt.split(" "))
+
+    # create the tensor based on which prompt is longer
+    if count_prompt >= count_negative_prompt:
+        input_ids = pipeline.tokenizer(prompt, return_tensors="pt", truncation=False).input_ids.to(device)
+        shape_max_length = input_ids.shape[-1]
+        negative_ids = pipeline.tokenizer(negative_prompt, truncation=False, padding="max_length",
+                                          max_length=shape_max_length, return_tensors="pt").input_ids.to(device)
+
+    else:
+        negative_ids = pipeline.tokenizer(negative_prompt, return_tensors="pt", truncation=False).input_ids.to(device)
+        shape_max_length = negative_ids.shape[-1]
+        input_ids = pipeline.tokenizer(prompt, return_tensors="pt", truncation=False, padding="max_length",
+                                       max_length=shape_max_length).input_ids.to(device)
+
+    concat_embeds = []
+    neg_embeds = []
+    for i in range(0, shape_max_length, max_length):
+        concat_embeds.append(pipeline.text_encoder(input_ids[:, i: i + max_length])[0])
+        neg_embeds.append(pipeline.text_encoder(negative_ids[:, i: i + max_length])[0])
+
+    return torch.cat(concat_embeds, dim=1), torch.cat(neg_embeds, dim=1)
+
 @hook()
 async def warmup(context=None):
     global from_huggingface
@@ -64,6 +101,10 @@ async def warmup(context=None):
             pipeline = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 
     pipeline = pipeline.to("cuda")
+
+    pipeline.enable_sequential_cpu_offload() # my graphics card VRAM is very lowd
+
+
     if not local_model:
         pipeline.safety_checker = lambda images, **kwargs: (images, [False])
 
@@ -79,7 +120,10 @@ async def text_to_image(prompt, negative_prompt='', model_id=None, from_huggingf
         await warmup()
 
     for n in range(1, count+1):
-        image = pipeline(prompt=prompt, negative_prompt=negative_prompt,
+
+        prompt_embeds, negative_prompt_embeds = get_pipeline_embeds(pipeline, prompt, negative_prompt, "cuda")
+
+        image = pipeline(prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_prompt_embeds,
                          num_inference_steps=steps, guidance_scale=cfg).images[0]
         fname = "imgs/"+random_img_fname()
         image.save(fname)
