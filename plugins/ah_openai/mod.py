89ae60ee92c969a13a8e004e6ef8be51928d6d2f
Jason Livesay <runvnc@gmail.com>
2024-05-26 01:55:53 -0500

    update

--- plugins/ah_openai/mod.py
+++ plugins/ah_openai/mod.py
@@ -1,25 +1,28 @@
-import openai
 from ..services import service
 import os
+import openai
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+ 
+client = openai.AsyncOpenAI()
 
 @service(is_local=False)
 async def stream_chat(model, messages=[], context=None, num_ctx=2048, temperature=0.0, max_tokens=100, num_gpu_layers=12):
     try:
-        #model = "cognitivecomputations/dolphin-2.5-mixtral-8x7b"
-        model = "meta-llama/Llama-3-70b-chat-hf"
-        openai.api_key = os.environ.get("OPENAI_API_KEY")
-        original_stream = openai.ChatCompletion.create(
-            model=model,
+        stream = await client.chat.completions.create(
+            model="gpt-4o",
+            stream=True,
             messages=messages,
             temperature=temperature,
             max_tokens=max_tokens
         )
+
         async def content_stream(original_stream):
-            for message in original_stream['choices'][0]['message']['content']:
-                yield message['content']
+            for message in stream['choices'][0]:
+                yield message.delta.content or ""
 
         return content_stream(original_stream)
 
     except Exception as e:
-        print('together.ai error:', e)
+        print('openai error:', e)
 
