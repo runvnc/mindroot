752b6d1077803b88234643f9d7175e1effdacf66
Jason Livesay <runvnc@gmail.com>
2024-05-27 18:57:52 -0500

    update

--- plugins/ah_think/mod.py
+++ plugins/ah_think/mod.py
@@ -5,11 +5,18 @@ import json
 
 @command(is_local=False)
 async def think(thought_process, context=None):
-    """Think about the next thing to do or say systematically before responding with commands.
+    """Think about the next thing to do or say systematically before additional commands or replies.
+
+    Returns: None
+
+    You should issue other commands or output after this command.
 
     Example (rough abstraction, not literal):
 
-    { "think": "Since the user wants [X] and we currently know [Y], therefore [Z]. Then assuming [Z], we will need to [1], [2]. The best way to [1] is ..." }
+    [
+    { "think": "Since the user wants [X] and we currently know [Y], therefore [Z]. Then assuming [Z], we will need to [1], [2]. The best way to [1] is ..." },
+    { "say": "Here is the plan I have come up with [...]" }
+    ]
 
     Actual thoughts will be entirely dependant upon the situation and conversation.
     You should skip this for very trivial conversation turns that don't require reflection.
