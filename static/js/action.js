b2b21bce36325a2b49dc3cb27978c1397df29f58
Jason Livesay <runvnc@gmail.com>
2024-05-29 03:28:07 -0500

    update

---
+++ static/js/action.js
@@ -0,0 +1,66 @@
+import { LitElement, html, css } from '/static/js/lit-core.min.js';
+import {BaseEl} from './base.js';
+
+class ActionComponent extends BaseEl {
+  static properties = {
+    funcName: { type: String },
+    params: { type: Object },
+    result: { type: String }
+  }
+
+  static styles = [
+    css`
+      .param_name { font-weight: bold; }
+      .param_value { color: blue; }
+      .fn_result { margin-top: 10px; }
+      .action { border: 2px solid blue; padding: 10px; margin: 10px; border-radius: 5px; background-color: gray; }
+    `
+  ];
+
+  constructor() {
+    super();
+    this.funcName = '';
+    this.params = {};
+    this.result = '';
+  }
+
+  showFunctionCall(funcName, params, result) {
+    let paramshtml = '';
+    for (var key in params) {
+      paramshtml += `<span class="param_name">${key}</span> `;
+      paramshtml += `<span class="param_value">${params[key]}</span>  `;
+    }
+
+    let res = '';
+    if (result != '()') {
+      let lines = result.split("\n");
+      if (lines.length == 1) {
+        res = `<div class="fn_result"><pre><code class="font-mono whitespace-pre-wrap" >${result}</code></pre></div>`;
+      } else {
+        res = `
+        <details class="block fn_result">
+          <summary>${lines[0]} ...</summary>
+          <pre><code class="font-mono whitespace-pre-wrap" >${result}</code></pre>
+        </details>`;
+      }
+    }
+
+    let html = ` 
+    <div></div>
+    <div>
+      <div class="av"></div>
+      <div class="action block border-2 border-blue-500 p-4 m-2 rounded-lg bg-gray-800" >
+        ⚡  <span class="fn_name">${funcName}</span> ${paramshtml}
+        ${res}
+      </div>
+    </div>
+    `;
+    return html;
+  }
+
+  render() {
+    return html`${unsafeHTML(this.showFunctionCall(this.funcName, this.params, this.result))}`;
+  }
+}
+
+customElements.define('action-component', ActionComponent);
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,103 +1,100 @@
-import { LitElement, html, css } from '/static/js/lit-core.min.js'
+import { LitElement, html, css } from '/static/js/lit-core.min.js';
 import { unsafeHTML } from 'https://unpkg.com/lit-html/directives/unsafe-html.js';
-import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
-import {BaseEl} from './base.js'
+import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js';
+import { BaseEl } from './base.js';
+import './action.js';
 
 class Chat extends BaseEl {
-    static properties = {
-      sessionid: { type: String },
-      messages: []
-     }
-  
-    static styles = [
+  static properties = {
+    sessionid: { type: String },
+    messages: []
+  }
+
+  static styles = [
     css`
-   `
-  ]
+    `
+  ];
 
-    constructor(args) {
-      super()
-      console.log({args})
-      this.messages = []
-      console.log('Chat component created')
-      console.log(this)
-     }
+  constructor(args) {
+    super();
+    console.log({ args });
+    this.messages = [];
+    console.log('Chat component created');
+    console.log(this);
+  }
 
-    firstUpdated() {
-      console.log('First updated')
-      console.log('sessionid: ', this.sessionid)
-      this.sse = new EventSource(`/chat/${this.sessionid}/events`)
-      this.sse.addEventListener('new_message', this._aiMessage.bind(this))
-      this.sse.addEventListener('image', this._imageMsg.bind(this))
-      this.sse.addEventListener('partial_command', this._partialCmd.bind(this))
-    }
+  firstUpdated() {
+    console.log('First updated');
+    console.log('sessionid: ', this.sessionid);
+    this.sse = new EventSource(`/chat/${this.sessionid}/events`);
+    this.sse.addEventListener('new_message', this._aiMessage.bind(this));
+    this.sse.addEventListener('image', this._imageMsg.bind(this));
+    this.sse.addEventListener('partial_command', this._partialCmd.bind(this));
+  }
 
-    _addMessage(event) {
-      const { content, sender, persona } = event.detail
-      this.messages = [...this.messages, { content: marked(content), sender, persona }]
+  _addMessage(event) {
+    const { content, sender, persona } = event.detail;
+    this.messages = [...this.messages, { content: marked(content), sender, persona }];
 
-      if (sender === 'user') {
-        fetch(`/chat/${this.sessionid}/send`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
+    if (sender === 'user') {
+      fetch(`/chat/${this.sessionid}/send`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
         body: JSON.stringify({ message: content })
-        })
-      }
-    }
-
-    _partialCmd(event) {
-      console.log('Event received') 
-      console.log(event)
-      const data = JSON.parse(event.data)
-      if (this.messages[this.messages.length-1].sender != 'ai') {
-        console.log('adding message')
-        this.messages = [...this.messages, { content: '', sender: 'ai',
-                                            persona: data.persona }]
-      }
-
-     if (data.command == 'say') {
-        this.messages[this.messages.length-1].content += `<span>${data.chunk}</span>`
-     } else { 
-        this.messages[this.messages.length-1].content = data.command + ': ' + marked(data.so_far)
-     }
-      this.requestUpdate()
-    }
-
-    _aiMessage(event) {
-      console.log('Event received') 
-      console.log(event)
-      const data = JSON.parse(event.data)
-      console.log('aimessage', data)
-       this.messages = [...this.messages, { content: data.content,
-                                           persona: data.persona, sender: 'ai' }]
-    }
-
-    _imageMsg(event) {
-      console.log('Event received') 
-      console.log(event)
-      const data = JSON.parse(event.data)
-      const html = `<img src="${data.url}" alt="image">`
-      this.messages = [...this.messages, { content: html, sender: 'ai' }]
-    }
-
-
-    _render() {
-      return html`
-        <div class="chat-container">
-          SessionID: ${this.sessionid}
-          <div class="chat-log">
-            ${this.messages.map(({ content, sender, persona }) => html`
-              <chat-message sender="${sender}" class="${sender}" persona="${persona}">
-                ${unsafeHTML(content)}
-              </chat-message>
-            `)}
-          </div>
-          <chat-form @addmessage="${this._addMessage}"></chat-form>
-        </div>
-      `
+      });
     }
   }
 
-customElements.define('chat-ai', Chat)
+  _partialCmd(event) {
+    console.log('Event received');
+    console.log(event);
+    const data = JSON.parse(event.data);
+    if (this.messages[this.messages.length - 1].sender != 'ai') {
+      console.log('adding message');
+      this.messages = [...this.messages, { content: '', sender: 'ai', persona: data.persona }];
+    }
 
+    if (data.command == 'say') {
+      this.messages[this.messages.length - 1].content += `<span>${data.chunk}</span>`;
+    } else {
+      this.messages[this.messages.length - 1].content = html`<action-component funcName="${data.command}" .params="${data.params}" result="${data.so_far}"></action-component>`;
+    }
+    this.requestUpdate();
+  }
+
+  _aiMessage(event) {
+    console.log('Event received');
+    console.log(event);
+    const data = JSON.parse(event.data);
+    console.log('aimessage', data);
+    this.messages = [...this.messages, { content: data.content, persona: data.persona, sender: 'ai' }];
+  }
+
+  _imageMsg(event) {
+    console.log('Event received');
+    console.log(event);
+    const data = JSON.parse(event.data);
+    const html = `<img src="${data.url}" alt="image">`;
+    this.messages = [...this.messages, { content: html, sender: 'ai' }];
+  }
+
+  _render() {
+    return html`
+      <div class="chat-container">
+        SessionID: ${this.sessionid}
+        <div class="chat-log">
+          ${this.messages.map(({ content, sender, persona }) => html`
+            <chat-message sender="${sender}" class="${sender}" persona="${persona}">
+              ${unsafeHTML(content)}
+            </chat-message>
+          `)}
+        </div>
+        <chat-form @addmessage="${this._addMessage}"></chat-form>
+      </div>
+    `;
+  }
+}
+
+customElements.define('chat-ai', Chat);
