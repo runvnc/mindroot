b5f0d00c74a33ba2347b52deb8f8b8f2980b1e6e
Jason Livesay <runvnc@gmail.com>
2024-06-02 04:59:06 -0500

    update

--- ah/ah_img_tags/mod.py
+++ ah/ah_img_tags/mod.py
@@ -4,11 +4,13 @@ import json
 
 @hook()
 async def add_instructions(context=None):
-    # read in tag list from data/img_tags.txt
-    with open(os.path.join(os.path.dirname(__file__), 'data/img_tags.txt')) as f:
-        img_tags = f.read().splitlines()
-    # convert to comma separated string
-    img_tags = ', '.join(img_tags)
+    try:
+        with open('data/img_tags.txt') as f:
+            img_tags = f.read().splitlines()
+        # convert to comma separated string
+        img_tags = ', '.join(img_tags)
 
-    return f"## Tags for Image Descriptions\n\nWhen describing an image, select from the following tags:\n\n{img_tags}.\n\nIf you need to describe something not found in the tag list, you can just use common phrases."
-
+        return f"## Tags for Image Descriptions\n\nWhen describing an image, select from the following tags:\n\n{img_tags}.\n\nIf you need to describe something not found in the tag list, you can just use common phrases."
+    except Exception as e:
+        print("img_tags.txt not found in data/")
+    return ""
