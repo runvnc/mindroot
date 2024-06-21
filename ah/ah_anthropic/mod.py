14cb90accdce8618cc30779cd65d05df9d40bcea
Jason Livesay <runvnc@gmail.com>
2024-06-21 01:56:26 -0500

    update

--- ah/ah_anthropic/mod.py
+++ ah/ah_anthropic/mod.py
@@ -8,15 +8,20 @@ async def stream_chat(model, messages=[], context=None, num_ctx=200000, temperat
     try:
         model = "claude-3-5-sonnet-20240620"
         client = anthropic.AsyncAnthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
+        system = messages[0]['content']
+        messages = messages[1:]
         async with client.messages.stream(
                 model=model,
+                system=system,
                 messages=messages,
                 temperature=temperature,
                 max_tokens=max_tokens
         ) as original_stream:
-            async for chunk in original_stream.text_stream:
-                yield chunk
+            async def content_stream():
+                async for chunk in original_stream.text_stream:
+                    yield chunk
 
-
+            return content_stream()
+    
     except Exception as e:
         print('claude.ai error:', e)
