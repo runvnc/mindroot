97665287e56641c0692b88c3b7bca056fb5c515f
Jason Livesay <runvnc@gmail.com>
2024-07-19 15:55:03 -0500

    Implemented a startup hook to identify and track public routes in the application.

--- ah/ah_jwt_auth/mod.py
+++ ah/ah_jwt_auth/mod.py
@@ -4,5 +4,7 @@ from ah.route_decorators import public_route
 @hook()
 async def startup(app, context):
     for route in app.routes:
+        print(route)
+        print(dir(route))
         if hasattr(route.endpoint, '__public_route__'):
             public_routes.add(route.path)
