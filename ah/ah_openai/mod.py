ecd9b15da63c47cdf41c9f68438299679060dfff
Jason Livesa <runvnc@gmail.com>
2024-09-26 09:42:22 +0000

    update

--- ah/ah_openai/mod.py
+++ ah/ah_openai/mod.py
@@ -8,9 +8,20 @@ client = openai.AsyncOpenAI()
 
 @service()
 async def stream_chat(model, messages=[], context=None, num_ctx=2048, temperature=0.0, max_tokens=3724, num_gpu_layers=12):
+    print("stream_chat called")
+    print("model", model)
     try:
+        if model == 'o1-preview':
+            content = await sync_chat_o1(messages)
+
+            async def content_stream_():
+                yield content
+
+            return content_stream_()
+
         stream = await client.chat.completions.create(
             model="chatgpt-4o-latest",
+            #model="o1-preview",
             stream=True,
             messages=messages,
             temperature=temperature,
@@ -28,4 +39,22 @@ async def stream_chat(model, messages=[], context=None, num_ctx=2048, temperatur
 
     except Exception as e:
         print('openai error:', e)
+
+
+from openai import OpenAI
+sync_client = OpenAI()
+
+async def sync_chat_o1(messages):
+    messages_copy = messages.copy()
+    messages_copy[0]['role'] = 'user'
+    print("calling o1-preview")
+    print("messages_copy", messages_copy)
+    response = sync_client.chat.completions.create(
+        model="o1-preview",
+        messages = messages_copy
+    )
+    print("response from o1-preview received:")
+    response = response.choices[0].message.content
+    print(response)
+    return response
 
