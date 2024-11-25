851b5232aa6feea0fed45c0263a6db575cf5fe28
Jason Livesay <runvnc@gmail.com>
2024-11-25 07:51:46 -0600

    update

--- src/xingen/coreplugins/startup/mod.py
+++ src/xingen/coreplugins/startup/mod.py
@@ -2,10 +2,15 @@ from lib.providers.hooks import hook, hook_manager
 from lib.route_decorators import public_route, public_routes
 from starlette.routing import Mount
 import termcolor
+import os
 
 print("--- AH Startup ---")
 
 async def on_load(app):
     print(termcolor.colored("startup plugin calling startup() hook...", 'yellow', 'on_green'))
+
+    for dirs in ['data/context', 'data/chat']:
+        os.makedirs(dirs, exist_ok=True)
+
     await hook_manager.startup(app, context=None)
 
