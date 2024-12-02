b4f35c6bd7619a967bc5e720b8358fd654e0ab5e
Jason Livesay <runvnc@gmail.com>
2024-12-02 16:27:01 -0600

    update

--- src/xingen/coreplugins/login/router.py
+++ src/xingen/coreplugins/login/router.py
@@ -2,13 +2,37 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from lib.route_decorators import public_route
+import os
+
+router = APIRouter()
+
+templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
+templates = Jinja2Templates(directory=templates_dir)
+
+@router.get("/login", response_class=HTMLResponse)
+@public_route()
+async def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+x="""
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from lib.route_decorators import public_route
 from lib.templates import render
 import os
 
 router = APIRouter()
+
+this_path = os.path.dirname(os.path.realpath(__file__))
+templates = Jinja2Templates(directory=os.path.join(this_path, "templates"))
+
 
 @router.get("/login", response_class=HTMLResponse)
 @public_route()
 async def login_page(request: Request):
     html = await render('login', {"request": request })
     return html
+"""
+
