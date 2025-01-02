91a7d7dfdc8b1d4f8993551ff4d6656b486c8a38
Jason Livesay <runvnc@gmail.com>
2025-01-01 23:53:26 -0600

    update

--- src/mindroot/coreplugins/smtp_email/mod.py
+++ src/mindroot/coreplugins/smtp_email/mod.py
@@ -26,7 +26,7 @@ class EmailMessage(BaseModel):
     html: bool = True
 
 @service()
-async def send_email(message: EmailMessage) -> bool:
+async def send_email(message: EmailMessage, context=None) -> bool:
     """Send an email using SMTP
     
     Args:
