f2b1b6bef8895d048d395eb67cfb85f5dfb300dc
Jason Livesay <runvnc@gmail.com>
2025-02-02 20:33:22 -0600

    messages text concat util; deepseek and tavily in default index

---
+++ src/mindroot/lib/utils/messages.py
@@ -0,0 +1,42 @@
+import json
+
+def concat_text_lists(message):
+    """Concatenate text lists into a single string"""
+    # if the message['content'] is a list
+    # then we need to concatenate the list into a single string
+    out_str = ""
+    if isinstance(message['content'], str):
+        return message
+    else:
+        for item in message['content']:
+            if isinstance(item, str):
+                if len(out_str) > 0:
+                    out_str += "\n"
+                out_str += item
+            else:
+                if len(out_str) > 0:
+                    out_str += "\n"
+                out_str += item['text']
+    message.update({'content': out_str})
+    return message
+
+def concat_all_texts(messages):
+    json_str = json.dumps(messages)
+    copy = json.loads(json_str)
+    return [concat_text_lists(m) for m in copy]
+
+
+def x(messages):
+    out = concat_all_texts(messages)
+    print(out) 
+
+def test():
+    messages = [ 
+        { "content": [ { "text": "a"}, { "text" :"c" } ] }, 
+          { "content": "b" } 
+    ]
+    x(messages)
+    print(messages)
+
+#test()
+
